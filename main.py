dff9f18545f8b7d76a2de8de05da748834a13e60
Kyle A McGee <kyle.mcgee34@gmail.com>
2023-05-08 13:02:49 -0400

    test run

--- main.py
+++ main.py
@@ -11,45 +11,50 @@ headers = {
     'CF-Access-Client-Secret': st.secrets["clientSecret"]
 }
 
-prompt = "In order to make homemade bread, follow these steps:\n1)"
+with st.sidebar:
+    temperature = st.slider('Temperature', min_value = 0.01, max_value = 1.99, step = 0.01, value = 0.7, help = 'Randomness of sampling. High values can increase creativity but may make text less sensible. Lower values will make text more predictable but can become repetitious')
+    max_new_tokens = st.slider('Token length', min_value = 1, max_value = 2000, step = 1, value = 250, help = 'Number of tokens to generate. (4 characters is about one token)')
+    prompt = st.text_area('Enter Custom Prompt', help = 'You will get much better output if you provide examples of what you expect back.')
+# prompt = "In order to make homemade bread, follow these steps:\n1)"
 
-request = {
-    'prompt': prompt,
-    'max_new_tokens': 250,
-    'do_sample': True,
-    'temperature': 1.3,
-    'top_p': 0.1,
-    'typical_p': 1,
-    'repetition_penalty': 1.18,
-    'top_k': 40,
-    'min_length': 0,
-    'no_repeat_ngram_size': 0,
-    'num_beams': 1,
-    'penalty_alpha': 0,
-    'length_penalty': 1,
-    'early_stopping': False,
-    'seed': -1,
-    'add_bos_token': True,
-    'truncation_length': 2048,
-    'ban_eos_token': False,
-    'skip_special_tokens': True,
-    'stopping_strings': []
-}
+if st.button('Generate Text'):
+    request = {
+        'prompt': prompt,
+        'max_new_tokens': max_new_tokens,
+        'do_sample': True,
+        'temperature': temperature,
+        'top_p': 0.1,
+        'typical_p': 1,
+        'repetition_penalty': 1.18,
+        'top_k': 40,
+        'min_length': 0,
+        'no_repeat_ngram_size': 0,
+        'num_beams': 1,
+        'penalty_alpha': 0,
+        'length_penalty': 1,
+        'early_stopping': False,
+        'seed': -1,
+        'add_bos_token': True,
+        'truncation_length': 2048,
+        'ban_eos_token': False,
+        'skip_special_tokens': True,
+        'stopping_strings': []
+    }
 
-response = requests.post(URI, json=request, headers=headers)
+    response = requests.post(URI, json=request, headers=headers)
 
-if response.status_code == 200:
-    try:
-        json_data = json.loads(response.text)
-        result = json_data['results'][0]['text']
-        st.markdown(prompt + result)
-    except (json.JSONDecodeError, KeyError) as e:
-        print(f"Error parsing response: {e}")
-        print(response.text)
-elif response.status_code == 404:
-    print('404: Not Found')
+    if response.status_code == 200:
+        try:
+            json_data = json.loads(response.text)
+            result = json_data['results'][0]['text']
+            st.markdown(prompt + result)
+        except (json.JSONDecodeError, KeyError) as e:
+            st.markdown(f"Error parsing response: {e}")
+            st.markdown(response.text)
+    elif response.status_code == 404:
+        st.markdown('404: Not Found')
 
-elif response.status_code == 401:
-    print('Denied: Check your API key')
-else:
-    print(f"Unexpected status code: {response.status_code}")
+    elif response.status_code == 401:
+        st.markdown('Denied: Check your API key')
+    else:
+        st.markdown(f"Unexpected status code: {response.status_code}")
